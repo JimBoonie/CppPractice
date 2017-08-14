c5b1a297027efac5001933ba5f83cf17c2b122c3
JimBoonie <mcgough.mason@gmail.com>
2017-08-13 22:12:18 -0400

    Modified insertion_sort to accept a direction value for descending order;

--- Algorithms/insertion_sort.cpp
+++ Algorithms/insertion_sort.cpp
@@ -3,7 +3,7 @@
 #include "horse_utils.cpp"
 using namespace std;
 
-void InsertionSort(vector<int>&);
+void InsertionSort(vector<int>&, bool);
 
 int main()
 {
@@ -11,28 +11,41 @@ int main()
     vector<int> my_list(my_array, my_array+6);
 
     // display original sequence
-	cout << "Unsorted sequence: ";
-	horse::DisplayVector(my_list);
+    cout << "Unsorted sequence: ";
+    horse::DisplayVector(my_list);
     
-    InsertionSort(my_list);
+    InsertionSort(my_list, 0);
 
     // display sorted sequence
-	cout << "Sorted sequence: ";
-	horse::DisplayVector(my_list);
+    cout << "Sorted sequence: ";
+    horse::DisplayVector(my_list);
 }
 
-void InsertionSort(vector<int>& list)
+void InsertionSort(vector<int>& list, bool direction=1)
 {
     // perform insertion sort
     for(int j = 1; j < list.size(); j++)
     {
-    	int anchor = list[j];
-    	int i = j - 1;
-    	while(i >= 0 && list[i] > anchor) 
-    	{
-    		list[i + 1] = list[i];
-    		i--;
-    	}
-    	list[i + 1] = anchor;
+        int anchor = list[j];
+        int i = j - 1;
+        if(direction)
+        {
+            while(i >= 0 && list[i] > anchor) 
+            {
+                list[i + 1] = list[i];
+                i--;
+            }
+            list[i + 1] = anchor;
+        }
+        else
+        {
+            while(i >= 0 && list[i] < anchor) 
+            {
+                list[i + 1] = list[i];
+                i--;
+            }
+            list[i + 1] = anchor;
+            
+        }
     }
 }
