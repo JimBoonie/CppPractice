befceeb7d85b08e15842e94c048da01a85e3d958
JimBoonie <mcgough.mason@gmail.com>
2018-03-08 20:29:37 -0500

    Almost implemented quick_sort.cpp but there is a bug;

--- Algorithms/quick_sort.cpp
+++ Algorithms/quick_sort.cpp
@@ -5,8 +5,10 @@
 #include "horse_utils.cpp"
 using namespace std;
 
+int RandIdx(const int, const int);
+int RandomPartition(vector<int>&, int, int);
+void QuickSortRange(vector<int>&, int, int);
 void QuickSort(vector<int>&);
-int RandIdx(const vector<int>&);
 
 int main()
 {
@@ -24,13 +26,53 @@ int main()
     horse::DisplayVector(my_list);
 }
 
-int RandIdx(const vector<int>& list)
+int RandIdx(const int lower, const int upper)
 {
 	srand(time(NULL));
-	return rand() % list.size();
+	return rand() % (upper + 1) + lower;
+}
+
+int RandomPartition(vector<int>& list, int p, int r)
+{
+    // find pivot point
+    int rand_idx = RandIdx(p, r);
+
+    // swap pivot point with end of list
+    int tmp = list[r];
+    list[r] = list[rand_idx];
+    list[rand_idx] = tmp;
+
+    // sort partition
+    int pivot_val = list[r];
+    int i = p - 1;
+    for(int j = p; j < r; j++)
+    {
+        if(list[j] <= pivot_val)
+        {
+            i += 1;
+            tmp = list[j];
+            list[j] = list[i];
+            list[i] = tmp;
+        }
+    }
+    i += 1;
+    tmp = list[r];
+    list[r] = list[i];
+    list[i] = tmp;
+    return i;
+}
+
+void QuickSortRange(vector<int>& list, int p, int r)
+{
+    if(p < r)
+    {
+        int q = RandomPartition(list, p, r);
+        QuickSortRange(list, p, q - 1);
+        QuickSortRange(list, q + 1, r);
+    }
 }
 
 void QuickSort(vector<int>& list)
 {
-    cout << RandIdx(list) << "\n";
+    QuickSortRange(list, 0, list.size() - 1);
 }
